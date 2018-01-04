e24d2a8caea0e34146a23b804c35f6e622fd381e
Matthew Hird <mhird13@gmail.com>
2018-01-04 15:14:26 -0800

    Day 4 solution (part 1 and 2).

--- 2017/Matt/days/day_04.py
+++ 2017/Matt/days/day_04.py
@@ -1,10 +1,25 @@
 
 class Day04:
     def __init__(self):
-        pass
+        self.pass_file = 'data/Day 4.txt'
+        self.count_passphrase = 0
 
     def part_1(self):
-        pass
+        with open(self.pass_file) as f:
+            for line in f:
+                phrase = line.split()
+                if len(phrase) == len(set(phrase)):
+                    self.count_passphrase += 1
+
+        print(self.count_passphrase)
 
     def part_2(self):
-        pass
+        with open(self.pass_file) as f:
+            for line in f:
+                phrase = line.split()
+                phrase = [''.join(sorted(word, reverse=True)) for word in phrase]
+
+                if len(phrase) == len(set(phrase)):
+                    self.count_passphrase += 1
+
+        print(self.count_passphrase)
