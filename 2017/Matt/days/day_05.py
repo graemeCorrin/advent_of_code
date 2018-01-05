d63286f6dd4afcb48017b73779293335d0cb6a62
Matthew Hird <mhird13@gmail.com>
2018-01-05 11:49:56 -0800

    Day 5 solution (part 1 and 2).

--- 2017/Matt/days/day_05.py
+++ 2017/Matt/days/day_05.py
@@ -1,10 +1,39 @@
 
 class Day05:
     def __init__(self):
-        pass
+        self.input = 'data/Day 5.txt'
+        self.numbers = []
 
     def part_1(self):
-        pass
+        with open(self.input) as f:
+            for line in f:
+                strip_int = int(line.rstrip())
+                self.numbers.append(strip_int)
+        length = len(self.numbers)
+        count = 0
+        distance = 0
+        while distance < length:
+            count += 1
+            hop = self.numbers[distance]
+            self.numbers[distance] += 1
+            distance += hop
+        print(count)
 
     def part_2(self):
-        pass
+        with open(self.input) as f:
+            for line in f:
+                strip_int = int(line.rstrip())
+                self.numbers.append(strip_int)
+        length = len(self.numbers)
+        count = 0
+        distance = 0
+        while distance < length:
+            count += 1
+            hop = self.numbers[distance]
+            if self.numbers[distance] >= 3:
+                self.numbers[distance] += -1
+            elif self.numbers[distance] < 3:
+                self.numbers[distance] += 1
+            distance += hop
+        print(count)
+
