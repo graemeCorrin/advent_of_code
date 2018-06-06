e252b618a1ed24a623cfefd58814b728098d441f
Unknown <mhird13@gmail.com>
2018-06-06 14:08:29 -0700

    Day 4 Solution Java

---
+++ 2017/Matt/Java/src/html/advent2017/Day4.java
@@ -0,0 +1,99 @@
+package html.advent2017;
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+import static java.util.Arrays.sort;
+
+class Day4 {
+  private String dataFile = "data/day4";
+
+
+  void solution() {
+    String[] data = getData();
+    System.out.println(part1(data));
+    System.out.println(part2(data));
+  }
+
+
+  private String[] getData() {
+    String line;
+    int lineCount = 0;
+    String[] lines = new String[1000];
+
+    try {
+      FileReader fRead = new FileReader(dataFile);
+      BufferedReader bRead = new BufferedReader(fRead);
+      while ((line = bRead.readLine()) != null) {
+        lines[lineCount] = line;
+        lineCount++;
+      }
+      bRead.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    String[] entries = new String[lineCount];
+    System.arraycopy(lines, 0, entries, 0, lineCount);
+
+    return entries;
+  }
+
+
+  private int part1(String[] data) {
+    int validCount = 0;
+    int lineCount = data.length;
+    boolean validPass;
+
+    for (int i = 0; i < lineCount; i++) {
+      String[] line = data[i].split(" ");
+      int wordCount = line.length;
+      validPass = true;
+
+      for (int j = 0; j < wordCount; j++) {
+        for (int k = j + 1; k < wordCount; k++) {
+          if (line[j].equals(line[k])) {
+            validPass = false;
+          }
+        }
+      }
+      if (validPass) {
+        validCount++;
+      }
+    }
+    return validCount;
+  }
+
+
+  private int part2(String[] data) {
+    int validCount = 0;
+    int lineCount = data.length;
+    boolean validPass;
+
+    for (int i = 0; i < lineCount; i++) {
+      String[] line = data[i].split(" ");
+      int wordCount = line.length;
+      validPass = true;
+
+      for (int j = 0; j < wordCount; j++) {
+        String[] word = line[j].split("");
+        Arrays.sort(word);
+        line[j] = Arrays.toString(word);
+      }
+
+      for (int j = 0; j < wordCount; j++) {
+        for (int k = j + 1; k < wordCount; k++) {
+          if (line[j].equals(line[k])) {
+            validPass = false;
+          }
+        }
+      }
+      if (validPass) {
+        validCount++;
+      }
+    }
+    return validCount;
+  }
+}
--- 2017/Matt/Java/src/html/advent2017/Main.java
+++ 2017/Matt/Java/src/html/advent2017/Main.java
@@ -3,7 +3,7 @@ package html.advent2017;
 public class Main {
 
   public static void main(String[] args) {
-	Day3 day = new Day3();
+	Day4 day = new Day4();
 	day.solution();
   }
 }
