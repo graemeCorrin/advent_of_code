0841f3bac24551d8118750b976b1390c4101c652
Matthew Hird <mhird13@gmail.com>
2018-12-02 00:50:46 -0800

    Create file structure for Advent of Code 2018

---
+++ 2016/Matt/Kotlin/src/Day10.kt
@@ -0,0 +1,36 @@
+package html.advent2016
+
+class Day10 : DayBase() {
+    private val dataFile = "data/day10"
+    private val valueA = 61
+    private val valueB = 17
+
+    override fun solution() {
+
+    }
+//        val data = getData()
+//        println(part1(data))
+//        println(part2(data))
+//    }
+//
+//
+//    private fun getData(): {
+//        return emptyList()
+//    }
+//
+//
+//    private fun part1(data: ): String {
+//        return ""
+//    }
+//
+//
+//    private fun part2(data: ): String {
+//        return ""
+//    }
+}
+
+
+fun main(args: Array<String>) {
+    val day = Day10()
+    day.solution()
+}
